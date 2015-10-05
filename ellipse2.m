37b4746b2fd995b1ccd76c889e1dc1f2974268b9
Chas Nelson <carl.nelson@durham.ac.uk>
2015-10-05 10:14:54 +0100

    Debugging Exp5 par loop - memory issue

--- ellipse2.m
+++ ellipse2.m
@@ -19,13 +19,13 @@ function bw = ellipse2(sz,pos,major,minor,phi)
 % bw = ellipse2(20,10,6,2), creates an image of size 20x20 with an ellipse
 % centred at x=10, y=10 of size 6 (major) by 2 (minor) and orientated along
 % the x-axis
-% bw = ellipse2([100,50],[20,40],10,5,57), creates an image of size 100x50 
-% with an ellipse centred at x=20, y=40 of size 10 (major) by 5 (minor) 
+% bw = ellipse2([100,50],[20,40],10,5,57), creates an image of size 100x50
+% with an ellipse centred at x=20, y=40 of size 10 (major) by 5 (minor)
 % nd orientated along 57 degrees to the x-axis
 % the x-axis
 %
 % Copyright 2015 Carl J. Nelson, Durham University, UK
-% 
+%
 % License   See included <a href="./LICENSE/">file</a> or visit
 %           <a href="https://github.com/ChasNelson1990/...
 %              A-New-Method-for-Ellipse-Detection-2015/">The GitHub
@@ -47,7 +47,12 @@ bw = zeros(m,n);
 %% Circumference & Parameterisation in Theta
 h = (major-minor)^2/(major+minor)^2;
 circ = pi*(major+minor)*(1+((3*h)/(10+sqrt(4-(3*h)))));%Approximate (Ramanujan, 1914)
-theta = linspace(0,2*pi,100*round(circ));
+try
+    theta = linspace(0,2*pi,100*round(circ));
+catch
+    disp([major, minor])
+    disp(circ)
+end
 %% Calculate Ellipse Outline Pixels
 x = x0 + 0.5*(major*cos(theta)*cosd(-phi) - minor*sin(theta)*sind(-phi));
 y = y0 + 0.5*(major*cos(theta)*sind(-phi) + minor*sin(theta)*cosd(-phi));
